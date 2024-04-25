b9d476c3de8348d8dd4185cbca194771426a4c4f
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-25 23:07:57 +0300

    Add new empty app with name social_media

--- social_media_service/settings.py
+++ social_media_service/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "account",
     "knox",
+    "account",
+    "social_media",
 ]
 
 MIDDLEWARE = [
