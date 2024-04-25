2a078f01eeccb79cb3b259a3876df1010cd39303
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-04-25 20:12:24 +0300

    Implement custom User model with registration via email

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+from account.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self) -> str:
+        return f"{self.pk} {self.email}"
